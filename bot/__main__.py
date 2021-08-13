524824c7d35af2da6059032640b5e99839248710
defrieantoe1999 <79563472+defrieantoe1999@users.noreply.github.com>
2021-08-13 21:01:03 +0700

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -234,7 +234,7 @@ def main():
     dispatcher.add_handler(stats_handler)
     dispatcher.add_handler(log_handler)
     updater.start_polling(drop_pending_updates=IGNORE_PENDING_REQUESTS)
-    LOGGER.info("Bot Started!")
+    LOGGER.info("!!GASS BOT URIP!!")
     signal.signal(signal.SIGINT, fs_utils.exit_clean_up)
 
 app.start()
