9b8076cf9b3b082d068fa7711724bb5e618ab616
defrieantoe1999 <79563472+defrieantoe1999@users.noreply.github.com>
2021-08-17 17:44:42 +0700

    Update wserver.py

--- wserver.py
+++ wserver.py
@@ -30,17 +30,23 @@ ul {
 li {
   margin: 10px 0;
 }
+
+
 p { font-size: 12px; margin: 24px;}
 </style>
 </head>
 <body>
 <h1>slam-mirrorbot: <a href="https://github.com/breakdowns/slam-mirrorbot">@Github</a></h1>
 <form action="{form_url}" method="POST">
+
 {My_content}
+
 <input type="submit" name="Select these files ;)">
 </form>
+
 <script>
 $('input[type="checkbox"]').change(function(e) {
+
   var checked = $(this).prop("checked"),
       container = $(this).parent(),
       siblings = container.siblings();
@@ -53,36 +59,49 @@ $('input[type="checkbox"]').change(function(e) {
     indeterminate: false,
     checked: checked
   });
+
   function checkSiblings(el) {
+
     var parent = el.parent().parent(),
         all = true;
+
     el.siblings().each(function() {
       let returnValue = all = ($(this).children('input[type="checkbox"]').prop("checked") === checked);
       return returnValue;
     });
     
     if (all && checked) {
+
       parent.children('input[type="checkbox"]').prop({
         indeterminate: false,
         checked: checked
       });
+
       checkSiblings(parent);
+
     } else if (all && !checked) {
+
       parent.children('input[type="checkbox"]').prop("checked", checked);
       parent.children('input[type="checkbox"]').prop("indeterminate", (parent.find('input[type="checkbox"]:checked').length > 0));
       checkSiblings(parent);
+
     } else {
+
       el.parents("li").children('input[type="checkbox"]').prop({
         indeterminate: true,
         checked: false
       });
+
     }
+
   }
+
   checkSiblings(container);
 });
 </script>
 </body>
 </html>
+
 """
 
 code_page = """
